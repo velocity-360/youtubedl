75da21a8457a9c07edc25b60c4671730842743fb
fullstack360 <info@fullstack360.com>
2017-02-10 13:47:17 -0500

    respond to twilio with stream

--- routes/convert.js
+++ routes/convert.js
@@ -1,48 +1,15 @@
 var express = require('express')
 var router = express.Router()
-var ytdl = require('ytdl-core')
-var FFmpeg = require('fluent-ffmpeg')
-var through = require('through2')
-var xtend = require('xtend')
-var fs = require('fs')
-
-function streamify (uri, opt) {
-  opt = xtend({
-    videoFormat: 'mp4',
-    quality: 'lowest',
-    audioFormat: 'mp3',
-    applyOptions: function () {}
-  }, opt)
-
-  var video = ytdl(uri, {filter: filterVideo, quality: opt.quality})
-
-  function filterVideo (format) {
-    return format.container === (opt.videoFormat)
-  }
-
-  var stream = opt.file ? fs.createWriteStream(opt.file) : through()
-
-  var ffmpeg = new FFmpeg(video)
-  opt.applyOptions(ffmpeg)
-  var output = ffmpeg
-    .format(opt.audioFormat)
-    .pipe(stream)
-
-  output.on('error', video.end.bind(video))
-  output.on('error', stream.emit.bind(stream, 'error'))
-  return stream
-}
+var Streamify = require('../utils/Streamify')
 
 router.get('/', function(req, res, next) {
     try {
       var url = req.query.video
-
-
       res.setHeader('Content-disposition', 'attachment; filename=file.mp3')
       res.setHeader('Content-type', 'audio/mpeg')
 
 //      streamify('https://www.youtube.com/watch?v=wnNb7ZJiLQE').pipe(res)
-      streamify(url).pipe(res)
+      Streamify.streamify(url).pipe(res)
     } catch (exception) {
       res.status(500).send(exception)
     }
--- routes/twilio.js
+++ routes/twilio.js
@@ -3,6 +3,7 @@ var router = express.Router()
 var accountSid = 'AC817c36f0cdb7e4d489c5e2586a149095'
 var authToken = 'fd2430d88835fab8df742d83818eafbd'
 var twilio = require('twilio')
+var Streamify = require('../utils/Streamify')
 
 
 router.get('/', function(req, res, next) {
@@ -20,8 +21,51 @@ router.get('/', function(req, res, next) {
     res.send('respond with a resource')
 })
 
+// webhook from twilio:
 router.post('/', function(req, res, next) {
-	var client = new twilio.RestClient(accountSid, authToken)
+	// TWILIO: {"ToCountry":"US","ToState":"NY","SmsMessageSid":"SM0a1de785280d9cce83cd5585741354b7",
+	// "NumMedia":"0","ToCity":"NEW YORK","FromZip":"10128",
+	// "SmsSid":"SM0a1de785280d9cce83cd5585741354b7","FromState":"CT","SmsStatus":"received",
+	// "FromCity":"NORWALK","Body":"Test task","FromCountry":"US","To":"+16467130087","ToZip":"10028",
+	// "NumSegments":"1","MessageSid":"SM0a1de785280d9cce83cd5585741354b7",
+	// "AccountSid":"AC817c36f0cdb7e4d489c5e2586a149095","From":"+12037227160","ApiVersion":"2010-04-01"}
+
+	// tiIjXGj2u8g
+	var message = req.body['Body']
+	var parts = message.split('?v=')
+	if (parts.length < 2){
+		//error
+	}
+
+	var youtubeId = parts[1]
+	if (youtubeId.length < 11){
+		//error
+	}
+
+	youtubeId = youtubeId.substring(11)
+	var url = 'https://www.youtube.com/watch?v='+youtubeId
+
+    try {
+		var url = req.query.video
+		res.setHeader('Content-disposition', 'attachment; filename=file.mp3')
+		res.setHeader('Content-type', 'audio/mpeg')
+
+//		Streamify.streamify(url).pipe(res)
+		var stream = Streamify.streamify(url)
+
+		var client = new twilio.RestClient(accountSid, authToken)
+		client.messages.create({
+		    body: stream,
+		    to: '+12037227160',  // Text this number
+		    from: '+16467130087' // From a valid Twilio number
+		}, function(err, message) {
+		    console.log(message.sid)
+		})
+    }
+    catch (exception) {
+		res.status(500).send(exception)
+    }
+
 
 
 })
