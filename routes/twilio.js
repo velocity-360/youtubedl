021f3e50558803f36ea79d5da4a5f0ee1176f707
fullstack360 <info@fullstack360.com>
2017-02-11 04:41:36 -0500

    test

--- routes/twilio.js
+++ routes/twilio.js
@@ -64,8 +64,9 @@ router.post('/', function(req, res, next) {
 
 		var client = new twilio.RestClient(accountSid, authToken)
 		client.messages.create({
+		    body: 'TEST',
 //		    body: stream,
-			media: 'http://i.imgur.com/jCEv2pg.jpg',
+//			media: 'http://i.imgur.com/jCEv2pg.jpg',
 		    to: '+12037227160',  // Text this number
 		    from: '+16467130087' // From a valid Twilio number
 		}, function(err, message) {
