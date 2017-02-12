0e50f39b1d8876356fc23cc8c2fe88e5c19feae6
fullstack360 <info@fullstack360.com>
2017-02-12 11:42:54 -0500

    write to file

--- routes/convert.js
+++ routes/convert.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var router = express.Router()
 var Streamify = require('../utils/Streamify')
+//var fs = require('fs')
+var path = require('path')
 
 router.get('/', function(req, res, next) {
     try {
@@ -9,7 +11,14 @@ router.get('/', function(req, res, next) {
       // res.setHeader('Content-type', 'audio/mpeg')
       // Streamify.streamify(url).pipe(res)
 
-      Streamify.streamify(url).pipe(res)
+	  var filePath = path.join(__dirname, 'public', 'file.mp3').replace('routes/', '')
+      var stream = Streamify.streamify(url, {file: filePath})
+//      console.log('STREAM: '+JSON.stringify(stream))
+
+      res.json({
+      	confirmation: 'success',
+      	path: '/file.mp3'
+      })
 
     } 
     catch (exception) {
--- utils/Streamify.js
+++ utils/Streamify.js
@@ -3,9 +3,9 @@ var FFmpeg = require('fluent-ffmpeg')
 var through = require('through2')
 var xtend = require('xtend')
 var fs = require('fs')
+var path = require('path')
 
 module.exports = {
-
 
 	streamify: function (uri, opt) {
 	  opt = xtend({
@@ -21,7 +21,11 @@ module.exports = {
 	    return format.container === (opt.videoFormat)
 	  }
 
+	  console.log('TEST: '+opt.file)
 	  var stream = opt.file ? fs.createWriteStream(opt.file) : through()
+
+	  // var filePath = path.join(__dirname, 'public', 'file.mp3').replace('utils/', '')
+	  // var stream = fs.createWriteStream(filePath)
 
 	  var ffmpeg = new FFmpeg(video)
 	  opt.applyOptions(ffmpeg)
