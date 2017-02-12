5c6a1490bf04f55c6ce1e712d6df15ba2893f053
fullstack360 <info@fullstack360.com>
2017-02-12 11:02:18 -0500

    render mp3 as tag

--- routes/convert.js
+++ routes/convert.js
@@ -5,11 +5,12 @@ var Streamify = require('../utils/Streamify')
 router.get('/', function(req, res, next) {
     try {
       var url = req.query.video
-      res.setHeader('Content-disposition', 'attachment; filename=file.mp3')
-      res.setHeader('Content-type', 'audio/mpeg')
+      // res.setHeader('Content-disposition', 'attachment; filename=file.mp3')
+      // res.setHeader('Content-type', 'audio/mpeg')
+      // Streamify.streamify(url).pipe(res)
 
-//      streamify('https://www.youtube.com/watch?v=wnNb7ZJiLQE').pipe(res)
       Streamify.streamify(url).pipe(res)
+
     } 
     catch (exception) {
       res.status(500).send(exception)
